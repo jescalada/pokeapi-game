f75debd503c1edb08d073e954dd137903558818b
Juan Escalada <juanescalada175@gmail.com>
2022-05-26 16:05:42 -0700

    Add user info and deletion functionality

--- public/admin.js
+++ public/admin.js
@@ -22,50 +22,29 @@ async function loadPokemonById(pokemonId) {
     }
 }
 
-async function loadProfile() {
+async function loadUserList() {
     let data = {
-        userId: userId,
+        isAdmin: isAdmin,
     }
 
-    fetch('/cart', {
+    fetch('/userlist', {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
             'Content-type': 'application/json'
         }
     }).then(response => response.json()).then(async (data) => {
-                $("#username").text(data.username);
-                $("#username2").text(data.username);
-                $("#username3").text(data.username);
-            data.past_orders.forEach(async (order, index) => {
-                let date = new Date(order[0].timestamp)
-                let dateTime = date.toString().split("GMT")
-                let element = `
-                    <div class="order" id="order-${index + 1}" style="text-align: center">
-                        <h3>Order id: #${index + 1}</h3>    
-                        <h2>${dateTime[0]}</h2>
-                        <p class="details">Total: $${order[0].total}</p>
-                        <h4>Items in Order #${index + 1}:</h4>
-                    </div>`;
-                $("#past-orders").append(element);
-
-                order[0].cart.forEach(async (pokemon) => {
-                    let pokemonData = await getPokemonBasicDataById(pokemon.pokemonId)
-                    let entry = `
-                    <div class="thumbnail-container" style="text-align: center; display: inline-block">
-                        <img src="${pokemonData.sprite}" alt="${pokemonData.name}" style="width:100%"
-                            onclick="location.href='pokemon.html?id=${pokemonData.id}'" class="pokemon-image-thumb">
-                            <div class="row pokemon-buy-details">
-                            <h3 class="col card-price">${pokemonData.name}</h3>
-                            <h3 class="col card-price">$${pokemonData.price}</h3>
-                            <h3 class="col card-quantity" id="card-quantity-${pokemonData.id}">Qty: ${pokemon.quantity}</h3>
-                            <h3 class="col card-total-price"> Total: $${(pokemonData.price * pokemon.quantity).toFixed(2)}</h3>
-                        </div>
-                    </div>
-                    `;
-                    $(`#order-${index + 1}`).append(entry);
-                })
-            });
+        console.log(data)
+            data.forEach(async (entry, index) => {
+            let element = `
+                <li class="user-entry row">
+                    <span class="user-info col">Id: #${entry.user_id}</span>
+                    <span class="user-info col">username: ${entry.username}</span>
+                    <span class="user-info col">is Admin?: ${entry.isAdmin ? "Yes" : "No"}</span>
+                    <button class="delete-user-button col" onclick="deleteUser(${entry.user_id})">Delete</button>
+                </li>    
+            `;
+            $("#user-entry-container").append(element);
         });
     })
 }
